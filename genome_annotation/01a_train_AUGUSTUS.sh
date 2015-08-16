6e616b9ac677757f362e74237d6871979f6458be
Travis <traviswrightsman@gmail.com>
2015-08-16 15:44:49 -0700

    skeleton structure of AUGUSTUS training script

---
+++ genome_annotation/01a_train_AUGUSTUS.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=4,mem=32gb,walltime=72:00:00
+
+module load augustus/3.0.3
+
+#Split gene structure set into training and test set
+
+
+#Create a meta parameters file
+
+
+#Make an initial training
+
+
+#Run optimize_augustus.pl
+
+
